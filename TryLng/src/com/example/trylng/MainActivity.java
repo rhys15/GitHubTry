fe7dd8886a8b560808fcca7e204af751bcdd2034
Rhys Gabion <rhys15gabion@gmail.com>
2013-06-02 17:24:37 +0800

    edit

--- TryLng/src/com/example/trylng/MainActivity.java
+++ TryLng/src/com/example/trylng/MainActivity.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	private String rhys = "rhys";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
